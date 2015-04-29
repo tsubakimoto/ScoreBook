fbd781718f98a239a506d48b71424a47695e5aae
tsubakimoto <tkmbmbr@gmail.com>
2015-04-29 12:18:27 +0900

    add initial project

---
+++ ScoreBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ScoreBook.Startup))]
+namespace ScoreBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
